51aeb3d45fdd7317da9f117b9b3ac313ba0cbcaa
AsokTamang <ashoktmg2058@gmail.com>
2025-12-31 11:32:12 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# so there are 2 types of machine learning
+# 1 statistical ML and deep learning
+# machine learning model is an equation or mathematical formulae that is derived from the operation , or comparison
+# or calculation between inputs and outputs from the large dataset
+# machine learning the part of computer science that trains the computer program on the datas so that
+# this program can make predictions without explicit programming.
+

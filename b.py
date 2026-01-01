f352d5d720f196ff3696ee308f345982b30efadb
AsokTamang <ashoktmg2058@gmail.com>
2026-01-01 01:01:30 -0500

    loading with json

--- b.py
+++ b.py
@@ -10,6 +10,7 @@ data = {'tom': {
         'phone': 123456
     }}
 
-with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'w') as f: #here we are writing in the file with w and here r means the string is raw string
-    (f.write(json.dumps(data)))
-
+with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'r') as f: #here we are writing in the file with w and here r means the string is raw string
+    a=(f.read())
+    finaldata = json.loads(a)   #here we are converting the string available in the given file path into JSON object again
+print(finaldata)

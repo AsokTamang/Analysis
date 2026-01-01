b2aed1bee2154a435273a738cc3b337ddd6db83f
AsokTamang <ashoktmg2058@gmail.com>
2026-01-01 00:54:45 -0500

    reading file

---
+++ b.py
@@ -0,0 +1,14 @@
+import json
+
+data = {'tom': {
+    'name': 'tom',
+    'age': 18,
+    'phone': 45612313},
+    'james': {
+        'name': 'james',
+        'age': 22,
+        'phone': 123456
+    }}
+
+with open(r'C:\Users\ashok\OneDrive\Desktop\change.txt', 'r') as f: #here we are writing in the file with w and here r means the string is raw string
+    print(f.read())

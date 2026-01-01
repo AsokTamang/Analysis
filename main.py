1c56eb25fc5ea62d30a4c960ec16fea0c980c6b9
AsokTamang <ashoktmg2058@gmail.com>
2026-01-01 00:20:04 -0500

    starting basics

--- main.py
+++ main.py
@@ -5,3 +5,20 @@
 # machine learning the part of computer science that trains the computer program on the datas so that
 # this program can make predictions without explicit programming.
 
+# there are two types of model training in statistical ML
+# classication and regression
+
+# in classification, the program classifies the output into different categories
+# there are binary and multiclass classification
+# binary classification is of two classes
+# multiclass classification is of multiple types
+
+# in regression, the program just provides the numerical output based on the input data
+
+
+
+# neural networks
+# A neural network is a layered model made of interconnected neurons that learn complex patterns in data through weighted mathematical operations.
+import os
+print(os.getcwd())
+print(os.mkdir('hello'))

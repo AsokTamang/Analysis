81f10b2320c3f8a6c67e6e22bdda91a6eedacca9
AsokTamang <ashoktmg2058@gmail.com>
2026-01-05 12:40:44 -0500

    fundamentals of numpy

--- allaboutnumpy/nump.py
+++ allaboutnumpy/nump.py
@@ -1,6 +1,9 @@
 import numpy as np
 import time
 
+#numpy arrays occupy less memory as well as they are much faster compared to the python lists
+#as well as numpy has a lot of built-in functions
+
 l1=np.arange(10)  #this np.arange creates the list upto 9
 print(l1)
 l2=np.arange(10)
@@ -8,3 +11,9 @@ start_time = time.time()
 l3 = l1 + l2   #vector addition of arrays
 endtime = time.time()
 print(endtime-start_time)
+
+rev=np.array([40,30,70])
+rev2=np.array([[40,30,70],[90,100,200]])
+print(rev.ndim)
+print(rev2.ndim)  #here .ndim gives us the number of dimension of an array
+print(rev2[1,1])  #based on indexing, we are getting the number

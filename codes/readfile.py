186df31a91c022e7fd619ed6e8b2790219d5fbef
AsokTamang <ashoktmg2058@gmail.com>
2026-01-04 21:42:16 -0500

    analaysis of total scores of players

--- codes/readfile.py
+++ codes/readfile.py
@@ -1,4 +1,14 @@
 with open("C:/Users/ashok/OneDrive/Desktop/change.txt",'r') as f:
      print(f.readlines())  #reading multiple lines
-with open ("C:/Users/ashok/OneDrive/Desktop/expenses.txt",'w') as f:
-     f.writelines(['I love to code\n','I love python and javascript']) #here \n is for line separator
+
+
+#calculating the total points scored by players in whole tournament
+with open("C:/Users/ashok/OneDrive/Desktop/change.txt",'r') as f:
+    next(f)  #here we are skipping the very first line cause the very first line consists of the headings
+    lines = f.readlines()
+    data = {}
+    for line in lines:
+        line.strip()  #removing empty space from the beginning and the end of the line
+        name,id,score = line.split('\t')
+        data[name] = data.get(name,0) + int(score)  #here we are assuming the 0 as the default score
+    print(data)

9f50ef9a25a50bb98470bc1f1955f03985d338cd
AsokTamang <ashoktmg2058@gmail.com>
2026-01-04 23:21:38 -0500

    final function

--- codes/exercisefile.py
+++ codes/exercisefile.py
@@ -1,24 +1,17 @@
-data={}
-with open("C:/Users/ashok/OneDrive/Desktop/customers.txt",'r') as f:
-    for line in f.readlines():
-     name,money=line.split(',')
-     data[name]=int(money)
-print(data)
+def process_customer_data(filename):
+    data = {}
+    customers_summary = {}
+    with open(filename, 'r') as f:
+        for line in f.readlines():
+            name, money = line.split(',')
+            data[name] = int(money)
+    for key, value in data.items():
+        if 100 <= value <= 199:
+            customers_summary[key] = (value, 'Bronze')
+        elif 200 <= value <= 499:
+            customers_summary[key] = (value, 'Silver')
+        else:
+            customers_summary[key] = (value, 'Gold')
+    return customers_summary
+print(process_customer_data("C:/Users/ashok/OneDrive/Desktop/customers.txt"))
 
-def calculate_rewards(total):
-    if 100<=total<=199:
-        return 'Bronze'
-    elif 200<=total<=499:
-        return 'Silver'
-    elif total>499:
-        return 'Gold'
-
-customers_summary = {}
-for key,value in data.items():
-    if 100<=value<=199:
-        customers_summary[key]=(value,'Bronze')
-    elif 200 <= value <= 499:
-        customers_summary[key]=(value,'Silver')
-    else:
-        customers_summary[key] = (value, 'Gold')
-print(customers_summary)

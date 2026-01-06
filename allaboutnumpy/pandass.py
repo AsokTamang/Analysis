8cf6a357df561a2b83174d387a371b910fea03bb
AsokTamang <ashoktmg2058@gmail.com>
2026-01-06 12:46:31 -0500

    getting the unique data from the list of datas

---
+++ allaboutnumpy/pandass.py
@@ -0,0 +1,10 @@
+import pandas as pd
+#in pandas we can access the data title from an Excel file as property
+import csv
+df=pd.read_excel("C:/pythonprojects/start.csv.xlsx")
+print(df.head(3))
+print(df.shape)  #so our excel file consists of 10 rows and 8 columns
+maximum_salary = df.salary.max()  #here we are getting the maximum value of the salary
+print(df[df['salary']==maximum_salary]['name'])  #here we are getting the name of the staff having maximum salary
+print(df.columns.astype(str))
+print(df.department.unique())  #printing the unique departments

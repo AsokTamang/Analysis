09b8062ce89211eca5f7c81454318a2ee5619cac
AsokTamang <ashoktmg2058@gmail.com>
2026-01-07 22:26:35 -0500

    using group by method to count the total number of bookings done in each platform

--- project/start.py
+++ project/start.py
@@ -1,4 +1,9 @@
 import pandas as pd
 df = pd.read_csv("C:/Users/ashok/OneDrive/Desktop/fact_bookings.csv")
 df.ratings_given.fillna(0,inplace=True)  #here the null value in ratings_given will be assigned 0 andwe must use inplace method
+total_booking_platform=df.groupby('booking_platform')
+#counting the total number of bookings done in each booking platform
+for k,v in total_booking_platform:
+    print(f'The total number of bookings done through {k} is', len(v))
+    print(v)
 df.head(10)

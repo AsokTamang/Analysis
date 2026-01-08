a226540c060c6c9a55146cc1cac759feb069b085
AsokTamang <ashoktmg2058@gmail.com>
2026-01-08 03:07:41 -0500

    operation 2

--- readwrite/start1.py
+++ readwrite/start1.py
@@ -9,3 +9,5 @@ maximum = df_agg_bookings.capacity.max()
 print(maximum)
 print(df_agg_bookings[df_agg_bookings['capacity']==maximum]['property_id'])  #here we are selecting the property id having the maximum capacity
 print(df_bookings.shape)
+df_total=pd.merge(df_agg_bookings[['successful_bookings','capacity','property_id']],df_bookings[['property_id','booking_date']],on='property_id')  #here we are taking only the required columns from the tables
+print(df_total[df_total['successful_bookings']>df_total['capacity']]['booking_date'])
